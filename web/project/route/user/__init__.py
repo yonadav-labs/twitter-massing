af815f0d104f64ab09939f8f2264530d7adda06a
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2018-03-23 11:06:57 +0530

    revise stylesheet and add select all

--- web/project/route/user/__init__.py
+++ web/project/route/user/__init__.py
@@ -4,6 +4,7 @@ user_routes = Blueprint('user_routes', __name__, url_prefix='/user')
 from .account import *
 from .profile import *
 from project import twitter_connection
+from twitter import Twitter, OAuth, TwitterHTTPError
 
 
 @user_routes.route('/findUsers', methods=['POST'])
@@ -17,26 +18,30 @@ def findUsers():
     current_app.logger.info(str(len(results['ids'])))
 
     for n in range(0, len(results["ids"]), 100):
-        ids = results["ids"][n:n + 100]
-        subquery = twitter_connection.users.lookup(user_id=ids)
-        for user in subquery:
+        try:
+            ids = results["ids"][n:n + 100]
+            subquery = twitter_connection.users.lookup(user_id=ids)
+            for user in subquery:
 
-            followings = user['friends_count']
-            followers = user['followers_count']
-            likes = user['favourites_count']
-            tweets = user['statuses_count']
+                followings = user['friends_count']
+                followers = user['followers_count']
+                likes = user['favourites_count']
+                tweets = user['statuses_count']
 
-            if (followings > int(json_data['followings_count']) and
-                        followers > int(json_data['followers_count']) and
-                        likes > int(json_data['likes_count']) and
-                        tweets > int(json_data['tweets_count'])):
-                tmpuser = {}
-                tmpuser['id'] = user['id']
-                tmpuser['name'] = user['name']
-                tmpuser['screen_name'] = user['screen_name']
-                tmpuser['location'] = user['location']
+                if (followings >= int(json_data['followings_count']) and
+                            followers >= int(json_data['followers_count']) and
+                            likes >= int(json_data['likes_count']) and
+                            tweets >= int(json_data['tweets_count'])):
+                    tmpuser = {}
+                    tmpuser['id'] = user['id']
+                    tmpuser['name'] = user['name']
+                    tmpuser['screen_name'] = user['screen_name']
+                    tmpuser['location'] = user['location']
 
-                users.append(tmpuser)
+                    users.append(tmpuser)
+            print len(users), '$#$#$#$#$#$#$#$#'
+        except TwitterHTTPError as api_error:
+            print api_error, '@@@@@@@@@@@@@@'
 
     result['users'] = users
     return jsonify({'result': result})
